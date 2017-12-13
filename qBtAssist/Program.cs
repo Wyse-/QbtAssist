7df831e6b0ba22c6ddc52381d1e88b02be1b5bb1
Wyse- <5454041+Wyse-@users.noreply.github.com>
2017-12-13 02:07:53 +0100

    Add files via upload

--- qBtAssist/Program.cs
+++ qBtAssist/Program.cs
@@ -8,13 +8,16 @@ namespace qBtAssist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(@"   ____  _     _                    _     _          __   ___  ");         //
-            Console.WriteLine(@"  / __ \| |   | |     /\           (_)   | |        /_ | / _ \ ");         //
-            Console.WriteLine(@" | |  | | |__ | |_   /  \   ___ ___ _ ___| |_  __   _| || | | |");         // ASCII art.
-            Console.WriteLine(@" | |  | | '_ \| __| / /\ \ / __/ __| / __| __| \ \ / / || | | |");         //
-            Console.WriteLine(@" | |__| | |_) | |_ / ____ \\__ \__ \ \__ \ |_   \ V /| || |_| |");         //
-            Console.WriteLine(@"  \___\_\_.__/ \__/_/    \_\___/___/_|___/\__|   \_/ |_(_)___/ ");         //
+            Console.WriteLine(@"   ____  _     _                    _     _          __  __ ");            //
+            Console.WriteLine(@"  / __ \| |   | |     /\           (_)   | |        /_ |/_ |");            //
+            Console.WriteLine(@" | |  | | |__ | |_   /  \   ___ ___ _ ___| |_  __   _| | | |");            // ASCII art.
+            Console.WriteLine(@" | |  | | '_ \| __| / /\ \ / __/ __| / __| __| \ \ / / | | |");            //
+            Console.WriteLine(@" | |__| | |_) | |_ / ____ \\__ \__ \ \__ \ |_   \ V /| |_| |");            //
+            Console.WriteLine(@"  \___\_\_.__/ \__/_/    \_\___/___/_|___/\__|   \_/ |_(_)_|");            //
+            Console.WriteLine();                                                                           //
+            Console.WriteLine(@"````````````````````````````````````````````````````````````");            //
 
+            bool isLoggedIn = false;
             string[] xmlData;                                                                              //
             string host = "";                                                                              //
             string username = "";                                                                          //
@@ -32,41 +35,59 @@ namespace qBtAssist
             downloadLimit = long.Parse(xmlData[4]);                                                        //
             processNames = new string[xmlData.Length - 5];                                                 //
 
-            for(int i = 5; i < xmlData.Length; i++)                                                        //
+            for (int i = 5; i < xmlData.Length; i++)                                                       //
             {                                                                                              // Populate processNames string array with the content of the <Process> nodes in the XML file.
                 processNames[i - 5] = xmlData[i];                                                          //
             }                                                                                              //
 
-            API.Initialize(host, 100);                                                                     //
-            if ((bool)API.Login(username, password).Result)                                                //
-            {                                                                                              //
-                Console.WriteLine("\n[Login successful.]\n");                                              //
-            }                                                                                              // Initialize Qbittorrent WebUI connection and attempt login with supplied info.
-            else                                                                                           //
-            {                                                                                              //
-                Console.WriteLine("[Login failed.]\n");                                                    //
-            }                                                                                              //
-
-            while (true)
-            {                                                                                              //
-                if (IsProcessRunning(processNames))                                                        //
-                {                                                                                          //
-                    Console.WriteLine("[Process running, capping upload & download.]");                    // Check if a process from the processNames array is running: if there's at least one process running set upload and download limit to supplied values.
-                    API.SetGlobalUploadSpeedLimit(uploadLimit);                                            //
-                    API.SetGlobalDownloadSpeedLimit(downloadLimit);                                        //
-                }                                                                                          //
+            while (true)                                                                                   // Infinite loop.
+            {
+                if (IsQbittorrentRunning())                                                                // Check if Qbittorrent is running; if it is proceed by attempting login to WebUI, otherwise display an error message and exit with code -1.
+                {
+                    API.Initialize(host, 100);                                                             //
+                    if (isLoggedIn = (bool)API.Login(username, password).Result)                           //
+                    {                                                                                      // Initialize Qbittorrent WebUI connection and attempt login with supplied info.
+                        Console.WriteLine("                           ");                                  //
+                        Console.SetCursorPosition(0, Console.CursorTop - 1);                               //
+                        Console.WriteLine("Login successful.");                                            //
+                        Console.SetCursorPosition(0, Console.CursorTop - 1);                               //
+                    }                                                                                      //
+                    else
+                    {                                                                                      //
+                        Console.WriteLine("Login failed, correct login info and retry.");                  // Display error message, wait for 5 seconds and exit with code -1.
+                        Thread.Sleep(5000);                                                                //
+                        Environment.Exit(-1);                                                              //
+                    }
+                }
                 else
-                {                                                                                          // 
-                    Console.WriteLine("[No process running, uncapping upload & download.]");               // 
-                    API.SetGlobalUploadSpeedLimit(0);                                                      // If no process is running uncap upload and download.
-                    API.SetGlobalDownloadSpeedLimit(0);                                                    // 
-                }                                                                                          // 
+                {
+                    Console.WriteLine("Qbittorrent is not running.");                                      //
+                    Console.SetCursorPosition(0, Console.CursorTop - 1);                                   // Write over previous line.
+                }
 
                 Thread.Sleep(5000);                                                                        // Sleep for 5 seconds.
 
-                Console.SetCursorPosition(0, Console.CursorTop - 1);                                       //
-                Console.WriteLine("                                                  ");                   // Clear line.
-                Console.SetCursorPosition(0, Console.CursorTop - 1);                                       //
+                while (IsQbittorrentRunning() && isLoggedIn)                                               //
+                {                                                                                          //
+                    if (IsProcessRunning(processNames))                                                    //
+                    {                                                                                      //
+                        Console.WriteLine("Process running, capping upload & download.");                  // Check if a process from the processNames array is running while Qbittorrent is running: if there's at least one process running set upload and download limit to supplied values.
+                        API.SetGlobalUploadSpeedLimit(uploadLimit);                                        // If Qbittorrent process ends (e.g. it crashes or is terminated by the user) attempt connection again.
+                        API.SetGlobalDownloadSpeedLimit(downloadLimit);                                    //
+                    }                                                                                      //
+                    else
+                    {                                                                                      // 
+                        Console.WriteLine("No process running, uncapping upload & download.");             // 
+                        API.SetGlobalUploadSpeedLimit(0);                                                  // If no process is running uncap upload and download.
+                        API.SetGlobalDownloadSpeedLimit(0);                                                // 
+                    }                                                                                      // 
+
+                    Thread.Sleep(5000);                                                                    // Sleep for 5 seconds.
+
+                    Console.SetCursorPosition(0, Console.CursorTop - 1);                                   //
+                    Console.WriteLine("                                                  ");               // Clear line.
+                    Console.SetCursorPosition(0, Console.CursorTop - 1);                                   //
+                }
             }
         }
 
@@ -88,6 +109,22 @@ namespace qBtAssist
             }
             return false;
             
+        }
+
+        /// <summary>
+        /// Determine if Qbittorrent is running.
+        /// </summary>
+        /// <returns>True if Qbittorrent is running, false if not.</returns>
+        static bool IsQbittorrentRunning()
+        {
+            if(Process.GetProcessesByName("qbittorrent").Length > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
